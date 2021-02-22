631ff7aa6b3580a828aa292729f12ec84d690b50
Alexander <alexander.david.olsson@gmail.com>
2021-02-22 22:55:31 +0100

    check

--- src/Checkout/AddressForm.js
+++ src/Checkout/AddressForm.js
@@ -7,7 +7,7 @@ export default function AddressForm(props) {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Shipping address
+        Leveransadress
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
